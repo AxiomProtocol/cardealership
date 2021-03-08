e78889954d7ec836605fa48383979c1dc9d304ce
AxiomProtocol <Galvanismreturns@protonmail.com>
2021-03-07 21:31:41 -0500

    implementadas autorizaciones, logout, etc

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 //Llamamos el modelo de la coleccion de usuarios
 const {User} = require('./models/user');
+//Llamamos al middleware the autorizacion
+const {auth} = require('./middleware/auth');
 //Llamamos a la llave para que se da la coneccion a la BD
 const config =require('./config/key');
 
 
 //Conectando a la BD
 mongoose.connect(config.mongoURI, 
-{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(() => console.log('Estamos conectados a la DB'))
+{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}).then(() => console.log('Estamos conectados a la DB'))
                                                     .catch(err => console.error(err));
 //Con esto evitamos las depreciaciones
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,23 +23,69 @@ app.use(bodyParser.json());
 //con esto activamos las galletas xd
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json({"funciona": "quizas si uwu"});
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        firstname: req.user.firstname,
+        lastname: req.user.lastname,
+        role: req.user.role
+    });
 });
 
-//Va a mostrar la pagina principal
-app.get('/', (req, res) => {
-    res.send('Funciona el Servidor');
-});
+
 
 //Con esto vamos a hacer que la informacion introducida al registrar usuarios se almacene y muestre
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
     
-    user.save((err, userData) => {
+    user.save((err, doc) => {
         if(err) return res.json ({success: false, err})
         return res.status(200).json({
-            success:true
+            success:true,
+            userData: doc
+        });
+    });
+});
+
+
+/*con esto nos vamos a asegurar de que el email y la clave del
+del usuario concuerden con lo que esta almacenado en su registro 
+dentro de la BD, permitiendole que inicie la sesion.
+Tambien general el token de identificacion*/
+app.post('/api/user/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if(!user)
+        return res.json({
+            loginSuccess: false,
+            message: "Email  incorrecta."
+        });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+            return res.json({
+                loginSuccess: false,
+                message: "Clave incorrecta."
+            });
+            
+        
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie("donotthinkkiddo", user.token).status(200)
+                    .json({
+                        loginSuccess: true
+                    });
+            });
+        });
+    });
+});
+
+//Con esta funcion permitios que el usuario termine sesion
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token:""}, (err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).send({
+            success: true
         });
     });
 });
--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 //Aqui se encuentra el modelo de base de datos para la informacion de usuarios.
 const mongoose = require('mongoose');
+//cantidad de veces que se encriptan las pates de la clave
+const saltRounds = 13;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-    //primer nombre del usuari
+    //primer nombre del usuario
     firstname: {
         type: String,
         maxlength: 50
@@ -21,7 +25,7 @@ const userSchema = mongoose.Schema({
     //clave del usuario
     password: {
         type: String,
-        minlength: 8
+        minglength: 8
     },
     //rol, si es administrador del sitio o usuario normal
     role: {
@@ -39,8 +43,62 @@ const userSchema = mongoose.Schema({
 
 });
 
+//Con esto vamos a hacer que la clave del usuario sea encriptada antes de guardarse a la bd
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) return next(err);
+        
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash
+                 /*Este next() se pone aqui para que el hashing se realice y se mande*/ 
+                 next();
+        });
+    });
+    
+    }
+    else{
+        next();
+    }
+});
+
+//haremos la comparacion de claves aqui
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+//Hacemos la generacion de token aqui
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'epsteinnosesuicidoxd')
+
+    user.token = token;
+    user.save(function (err, user){
+        if(err) return cb(err)
+        cb(null, user);
+    });
+}
+
+userSchema.statics.findByToken = function (token, cb){
+    var user = this;
+        //La decodificacion del token nos dara la ID del usuario, asi detectamos si esta autorizado o no
+    jwt.verify(token, 'epsteinnosesuicidoxd', function(err, decode){
+        user.findOne({"_id":decode, "token":token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user);
+        });
+    });
+}
+
+
 //Con esto se crea la coleccion que estara en la base de datos
-const User =mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 //Exportamos
 module.exports = {User};
