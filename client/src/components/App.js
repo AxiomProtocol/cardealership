43f520494b2e0f340364f5e9fdb884ff73d358ff
AxiomProtocol <Galvanismreturns@protonmail.com>
2021-03-08 01:05:03 -0500

    react agregado

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Pulpito dice hola ajajaja
+    </div>
+  );
+}
+
+export default App;
